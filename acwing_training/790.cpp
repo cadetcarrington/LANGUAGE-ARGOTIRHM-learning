575805d167032bcd19c780c0394d4afee5600b57
cadetcarrington <giggsgeorge258@gmail.com>
2026-01-15 21:02:54 +0800

    1.15 1(二分AC)

--- acwing_training/790.cpp
+++ acwing_training/790.cpp
@@ -7,7 +7,13 @@ int main (){
     cin >> n;
 
     double l = 0, r = n;
-    
+    if (r < 0){
+        swap(l, r);
+    }//如果n为负数，需要更改l和r的数值
+    if(n > -1 && n < 1){
+        l = -1;
+        r = 1;
+    }//对于在-1，1之间的数，需要特别的对待
     while (r - l >= 1e-8){
         double mid = (l + r ) / 2;
         if(mid * mid * mid >= n){
