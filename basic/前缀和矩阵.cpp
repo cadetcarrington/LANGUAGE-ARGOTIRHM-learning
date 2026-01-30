686a959d5cc16d205d19d050fe72ea16020b7091
cadetcarrington <giggsgeorge258@gmail.com>
2026-01-30 08:42:42 -0500

    D5v3

---
+++ basic/前缀和矩阵.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+const int N = 1010;
+
+int n, m, q;
+int a[N][N], s[N][N];
+
+int main (){
+    cin >> n >> m >> q;
+
+    for (int i = 1; i <= n; i ++){
+        for (int j = 1; j <= m; j ++){
+            scanf("%d", &a[i][j]);
+            s[i][j] = s[i - 1][j] + s[i][j - 1] - s[i - 1][j - 1] + a[i][j];
+        }
+    }
+
+    while (q --){
+        int x1, x2, y1, y2;
+        scanf("%d%d%d%d", &x1, &y1, &x2, &y2);
+        printf("%d\n", s[x1][y1] - s[x2][y1] - s[x1][y2] + s[x2][y2]);
+    }
+
+    return 0;
+}
