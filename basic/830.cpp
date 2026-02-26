f11469239af40b69e58bab63f006c1423a48a6ff
cadetcarrington <giggsgeorge258@gmail.com>
2026-02-26 05:12:47 -0500

    2.26_1

---
+++ basic/830.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+const int N = 100010;
+
+int n, stk[N], tt;
+
+int main (){
+    cin >> n;
+    
+    for (int i = 0; i < n; i ++){
+        int x;
+        cin >> x;
+
+        while (tt && stk[tt] >= x){
+            tt --;
+        }
+
+        if(tt) cout << stk[tt] << endl;
+        else cout << -1 << endl;
+        
+        stk[++ tt] = x;
+    }
+    return 0;
+}
