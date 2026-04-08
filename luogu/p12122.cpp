28760bbe6b226aaffe21d3834db7f46fd712b1ca
cadetcarrington <giggsgeorge258@gmail.com>
2026-04-08 20:29:56 +0800

    4.8.2

--- luogu/p12122.cpp
+++ luogu/p12122.cpp
@@ -1,26 +1,40 @@
 #include <iostream>
 #include <algorithm>
+#include <iomanip>
 
 using namespace std;
 
 int a[52];
+
+int shichuanling(){
+    int s = 0;
+    for (int i = 1; i <= 51; i ++){
+        for (int j = i + 1; j <= 51; j ++){
+            if(a[j] < a[i]) s ++;
+        }
+    }
+    return s;
+}
 
 int main (){
     for (int i = 1; i < 52; i ++){
         a[i] = i;
     }
 
+    int ans = 0, n = 0;
     for (int i = 1; i <= 51; i ++){
         for (int j = i + 1; j <= 51; j ++){
             for (int k = 1; k <= 51; k ++){
                 for (int l = k + 1; l <= 51; l ++){
                     swap(a[i], a[j]);
                     swap(a[k], a[l]);
-
-                    sort(a + 1, a + 51);
+                    n ++;
+                    ans += shichuanling();
+                    sort(a + 1, a + 52);
                 }
             }
         }
     }
+    cout << setprecision(10) << 1.0 * ans / n << endl;
     return 0;
 }
