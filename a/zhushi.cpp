0e148e429f5c5f10b94e526f0c9d4a67e51d26f2
cadetcarrington <giggsgeorge258@gmail.com>
2026-04-17 17:52:21 +0800

    4.16.2

--- a/zhushi.cpp
+++ a/zhushi.cpp
@@ -4,42 +4,64 @@ using namespace std;
 
 int main (){
     string a;
-    cin >> a;
 
-    int status = 0;
+    int flock = 0;
     while(getline(cin, a)){
-        int mao = 0, chang = 0, dan = 0, xing = 0, xie = 0;
-        for (auto c : a){
-            if(c == '"'){
-                if (!mao) mao ++;
-                else mao --;
-                continue;
-            }
-            if(mao == 1){
-                cout << c;   
-            }
-            else if (!xing){
-                // if(c == '/'){
-                //     if(dan == 1) dan ++;
-                //     if(!dan){
-                //         dan ++;
-                //     }
-                // }
-                // if(dan == 2){
-                //     cout << "#";
-                // }
-                if(c == '/' && (c + 1) == '/'){
-                    dan ++;
-                    c ++;
+        int mao = 0, dan = 0;
+        for (int i = 0; i < (int)a.size(); i++){
+            char c = a[i];
+            if(flock == 0){
+                if(c == '"' && !dan){
+                    if (!mao) mao ++;
+                    else mao --;
+                    cout << c;
                     continue;
                 }
-                if(dan){
+                if(mao == 1){
+                    cout << c;   
+                }
+                else {
+                    // if(c == '/'){
+                    //     if(dan == 1) dan ++;
+                    //     if(!dan){
+                    //         dan ++;
+                    //     }
+                    // }
+                    // if(dan == 2){
+                    //     cout << "#";
+                    // }
+                    if(!dan && i + 1 < (int)a.size() && c == '/' && a[i + 1] == '/'){
+                        dan ++;
+                        cout << c << a[i + 1];
+                        i ++;
+                        continue;
+                    }
+                    if(dan){
+                        cout << "#";
+                        continue;
+                    }
+                    if(i + 1 < (int)a.size() && c == '/' && a[i + 1] == '*'){
+                        cout << "/*";
+                        i ++;
+                        flock = 1;
+                        continue;
+                    }
+
+                    cout << c;
+                }
+            }
+            else {
+                if(i + 1 < (int)a.size() && c == '*' && a[i + 1] == '/'){
+                    cout << "*/";
+                    i ++;
+                    flock = 0;
+                }
+                else {
                     cout << "#";
-                    continue;
                 }
-                
             }
         }
+        cout << endl;
     }
 
     return 0;
